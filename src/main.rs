e502c6a870d13e72ebf72724a38fc38111b371b9
Ben Schubert <benschubert2002@gmail.com>
2024-12-17 20:52:57 -0600

    converted input reading to use key events

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
+extern crate termion;
+
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use termion::clear;
-use std::io::{stdin, stdout, Read, Write};
 
 fn main() {
     // Put the terminal into raw mode
-    let mut stdout = stdout().into_raw_mode().unwrap();
     let stdin = stdin();
-    stdout.activate_raw_mode().unwrap();
+    let mut stdout = stdout().into_raw_mode().unwrap();
 
     // Clear the screen
-    print!("{}", clear::All);
+    write!(
+        stdout, "{}{}", 
+        termion::clear::All,
+        termion::cursor::Goto(1, 1)
+    ).unwrap();
     stdout.flush().unwrap();
 
-    for b in stdin.bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-
-        // Print the recieved character to the console
-        print!("{}", c);
-        stdout.flush().unwrap();
-
-        // Quit the program when Ctrl-C is pressed
-        if b == 3 {
-            stdout.suspend_raw_mode().unwrap();
-            break;
+    for k in stdin.keys() {
+        match k.as_ref().unwrap() {
+            Key::Char(c) => print!("{}", c),
+            Key::Ctrl('c') => break,
+            _ => {
+                // Do nothing, for now. . .
+            }
         }
+        stdout.flush().unwrap();
     }
 }
